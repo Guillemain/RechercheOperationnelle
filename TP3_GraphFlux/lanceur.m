4f1eb0c0de7300b8b0837fb5da0e081d613647eb
Nicolas Vial <nicolas.vial@etu.enseeiht.fr>
2018-10-04 11:56:41 +0200

    lanceur

--- TP3_GraphFlux/lanceur.m
+++ TP3_GraphFlux/lanceur.m
@@ -7,3 +7,5 @@ LONG = [1 2 5 3 3 2 0 3 1 3 7 2 1 2 4];
 POS=[0 1 0; 3 0 4; 0 6 0; 5 0 8;0 7 0; 0 0 2]';
 csvwrite("./4test/pos_tension.csv", POS)
 
+[~, rep_courant]=system('pwd')
+status=unix(strcat(rep_courant,'/show_result.sh 3test/Depart.csv 3test/Result.csv 3test/Mat_P flot4'));
