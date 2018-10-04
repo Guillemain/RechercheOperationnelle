d9d842f38b79d0948f1b4b576ac7df9bd43d14f0
Portelli Gabriel <gportell@localhost.localdomain>
2018-10-04 11:33:16 +0200

    Matrice de Transition

--- TP3_GraphFlux/optiFlots.m
+++ TP3_GraphFlux/optiFlots.m
@@ -1,11 +1,11 @@
-function Resultat = optiFlots (A,depart,arrive)
+function Resultat = optiFlots (A,strat,depart,arrive)
 iterationMax = 50;
 nbIte = 0;
 beta = -1;
 As = A; Phi = zeros(size(A)); PhiN = zeros(size(A));
 
 while ((nbIte < iterationMax) && (beta(1) ~= 0))
-    [Phi,beta] = CreerChemin(As,2,depart,arrive);
+    [Phi,beta] = CreerChemin(As,strat,depart,arrive);
     As = As - beta(1)*Phi;
     PhiN = PhiN + beta(1) * Phi;
     nbIte = nbIte + 1;
--- TP5_Markov/Transition.m
+++ TP5_Markov/Transition.m
@@ -1,11 +1,12 @@
 function P = Transition(D, S, s)
-    V=fliplr(D);
+    V=fliplr(D(1:S+1));
     V(1)=sum(D(S:end));
     
     P=zeros(S+1,S+1);
-    P([[1:s],S+1],:)=V;
+    P([[1:s],S+1],:)=repmat(V,s+1,1);
     
     for i = s+1 : S 
         P(i,[2:i])=fliplr(D(1:i-1));
         P(i,1)=sum(D(i:end));  
+    end
 end
