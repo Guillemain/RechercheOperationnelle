511f40c747b904d6c614d5fbef44aa305fea641f
Pelissier-combescure Marie <mpelissi@localhost.localdomain>
2018-10-12 14:21:18 +0200

    1er tentative

--- TP5_Markov/Optimisation.m
+++ TP5_Markov/Optimisation.m
@@ -1,21 +1,29 @@
-function [sOptimal,SOptimal] = Optimisation((D,PI,v,C1,C2,C3,C4)
+function [sOptimal,SOptimal,matriceBenefice,M] = OptimisationV2(D,v,C1,C2,C3,C4)
 
 
-Smax = size(D,1);
-smax = size(D,1);
+Smax = size(D,2)-1;   % Smax = 6
+smax = size(D,2)-2;   % smax = 5
 
-matriceBenefice = zeros(smax,Smax);
+ligne = zeros(1,Smax);
+matriceBenefice = zeros(smax+1,Smax);
 
-for i = 1:Smax
-    for j = 1:smax+1
-        matriceBenefice(i,j) = benefice(D,i,j,PI,v,C1,C2,C3,C4)    
+for s = 0:smax
+    ligne = zeros(1,Smax);
+    for S = s+1:Smax
+        P = Transition(D, S, s);
+        Pi = DbLimite(P);
+        ligne(S) = benefice(D,S,s,Pi,v,C1,C2,C3,C4);
     end
+ 
+    matriceBenefice(s+1,:) = ligne;
+    
 end
 
 [M,I] = max(matriceBenefice(:));
 [ligneOptimale,colonneOptimale]= ind2sub(size(matriceBenefice),I);
 
-sOptimal = ligneOptimale - 1;
+M
+sOptimal = ligneOptimale -1;
 SOptimal = colonneOptimale;
 
 
--- TP5_Markov/benefice.m
+++ TP5_Markov/benefice.m
@@ -1,4 +1,4 @@
-function benef = benefice(D,S,s,PI,v,C1,C2,C3,C4)
+function benef = benefice(Dav,S,s,PI,v,C1,C2,C3,C4)
 
 
 % D vecteur colonne de probabilité de demande  alpha(k) = D(k+1)
@@ -11,14 +11,16 @@ function benef = benefice(D,S,s,PI,v,C1,C2,C3,C4)
 % benif benefice pour le couple (S,s)
 
 
-kmax = size(D,1); % Demande maximale = taille de D
-
+kmax = size(Dav,2); % Demande maximale = taille de D
+D = [Dav zeros(1,S+1+kmax)];
 
 % Calcul de somme2
 somme2 = 0;
-for k= 1:kmax
-    somme2 = somme2 + k*D(k+S+1);
-end
+%if (kmax-S-1) > 0
+    for k= 1:kmax %-S-1
+        somme2 = somme2 + k*D(k+S+1);
+    end
+%end 
 
 % Calcul somme3
 somme3 = 0;
@@ -29,37 +31,41 @@ end
 
 % Calcul somme1
 somme1 = 0;
-for j = 1:s
-   somme1 = somme1 + PI(j)*(-C1*S - C2*somme2 - C3 -C4*(S-j) + v*somme3);
-end
+if s>0
+    for j = 0:s-1
+        somme1 = somme1 + PI(j+1)*(-C1*S - C2*somme2 - C3 -C4*(S-j) + v*somme3);
+    end
+end 
 
 
 
 % Calcul somme4
 somme4 = 0;
-for j= s:S
+for j= s+1:S+1
    somme5 = 0;
    somme6 = 0;
    
     % Calcul somme5
     for k=1:kmax
-        somme5 = somme5 + k*D(k+j+1); 
+        somme5 = somme5 + k*D(k+j); 
     end
     
     % Calcul somme6
     for k = 1 : kmax
-        somme6 = somme6 + min(j,k)*D(k+1); 
+        somme6 = somme6 + min(j-1,k)*D(k+1); 
     end
    
-   somme4 =somme4 + PI(j)*(-C1*j - C2*somme5 + v*somme6); 
+   somme4 =somme4 + PI(j)*(-C1*(j-1) - C2*somme5 + v*somme6); 
    
 end
 
 
 % Calcul du benefice
-
-benef = somme1 + somme4;
-
+if s<S
+    benef = somme1 + somme4;
+else
+    benef = 0;
+end 
 
 
 
