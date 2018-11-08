519ae6c6853f889b342f21f55f8ee1e687d939d5
Canin Amelie <acanin@localhost.localdomain>
2018-11-08 16:54:29 +0100

    Commentaire sur strat_Amelie_2018 dans bancEssais (strat finale)

--- TP4/bancEssais/strat_Amelie_2018.m
+++ TP4/bancEssais/strat_Amelie_2018.m
@@ -19,11 +19,11 @@ if (numpart == 1) %premier coup joue
     x= 0.74998; %valeur pour se reconnaitre
     
 else
-    if (ty(1) == 0.74998)
+    if (ty(1) == 0.74998) %Si nous-même
         x = 0.75;
-    elseif (ty(1) == 2.9998)
+    elseif (ty(1) == 2.9998) %Si perteafond
         x = 1.5;
-    elseif (ty(numpart-1) == 0.75)
+    elseif (ty(numpart-1) == 0.75) %Si coopératif alors coopératif
         if (numpart == 100)
             x = 1.1250;
         else
@@ -35,26 +35,29 @@ else
 end
 end
 
+
+%Fonction auxiliaire pour détecter une stratégie affine
 function x = aux_antiA(numpart,tx,ty,gx,gy) 
 if numpart < 5 || isequal((ty(numpart-3:numpart-1) == ty(numpart-1)), ones(1,3))
-    % si on manque de données ou si y joue toujours la meme chose, non
-    % cooperatif
-	%s= strat_noncooperatif(numpart,tx,ty,gx,gy);
+    % si on manque de données ou si y joue toujours la meme chose, alors on
+    % joue sur antiaffine + random
     s = min(3,2*(3-ty(numpart-1))/3 +  (rand(1,1) -0.0) * 0.1);
 else
-    % si on detecte une stratégie affine, antiaffine sinon keran2
+    % si on detecte une stratégie affine, antiaffine sinon antiaffine + random
     tableauDonnees = [ty(numpart-3: numpart-1);tx(numpart-4: numpart-2)];
     tableauDonnees = unique(tableauDonnees','rows')';
+    %Calcul du potentiel coefficient de la stratégie affine
     coef = tableauDonnees(1,1)/(3-tableauDonnees(2,1));
     eps = 1e-10;
     utiliseAntiAffine = norm(tableauDonnees(1,:)-coef*(3-tableauDonnees(2,:))) < eps && length(tableauDonnees(1,:))>1 && coef<1 && coef>0;
     if utiliseAntiAffine
-        s = (3-ty(numpart-1))/(2-coef);
+        s = (3-ty(numpart-1))/(2-coef); %jeu anti-affine
     else
         s = min(3,2*(3-ty(numpart-1))/3 + (rand(1,1) -0.0) * 0.1);
     end
 end
 
+%Verification de la solution entre 0 et d
 if (s<0||s>=3)
     x = 0;
 else
